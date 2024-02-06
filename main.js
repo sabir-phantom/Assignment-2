f9419a8c83ab6812f1a051e2b050eed8f20f76a8
sabir-phantom <sabir.sbair@gmail.com>
2024-02-06 23:01:56 +0600

    file upload using multer

--- main.js
+++ main.js
@@ -3,6 +3,29 @@ const fs = require('fs');
 const multer = require('multer');
 const express = require('express');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, callBack) {
+        callBack(null, './uploads');
+    },
+    filename: function (req, file, callBack) {
+        callBack(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage }).single('myFIle');
+const app = express();
+app.post('/uploads', function (req, res) {
+    upload(req, res, function (error){
+        if (error) {
+            res.end("file upload error");
+        }
+        else {
+            res.end('file upload success');
+        }
+    });
+})
+
+
 let server = http.createServer(function(req, res) {
     // res.writeHead(200, {'Content-Type': 'text/plain'});
     // res.write('Hello World');
@@ -30,8 +53,12 @@ let server = http.createServer(function(req, res) {
             }
         });  
     } else if (req.url == '/file-upload') {
-        
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('file upload success');
+        res.end();
     }
+
+    res.end();
 
 }).listen(5500, function () {
     console.log("server listening on port 5500");
